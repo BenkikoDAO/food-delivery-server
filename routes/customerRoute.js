50c2fcb14d0dc9ed043efc83205a6d518dfcaf19
munene-m <munenenjue18@gmail.com>
2023-06-19 08:16:02 +0300

    added reset and update endpoint routes

--- routes/customerRoute.js
+++ routes/customerRoute.js
@@ -1,8 +1,10 @@
-import { createCustomer, loginCustomer } from "../controllers/customerController.js";
+import { createCustomer, loginCustomer, resetPassword, updatePassword } from "../controllers/customerController.js";
 import express from "express";
 const router = express.Router()
 
 router.route("/register").post(createCustomer)
 router.route("/login").post(loginCustomer)
+router.route("/reset").post(resetPassword)
+router.route("/update-password").put(updatePassword)
 
 export default router
