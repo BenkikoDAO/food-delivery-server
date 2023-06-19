554c28198896b70441d071aa80ac2f6ec81f9eff
munene-m <munenenjue18@gmail.com>
2023-06-19 08:43:27 +0300

    created the menu model

---
+++ models/menu.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+const menuSchema = new mongoose.Schema({
+    vendorID: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true
+    },
+    name: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type:String,
+        required: true
+    },
+    price: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+    }
+
+}, {timestamps: true})
+
+export default mongoose.model("Menu", menuSchema)
