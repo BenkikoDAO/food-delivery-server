2c01ca0447673797392eb63063e16984fbbff199
munene-m <munenenjue18@gmail.com>
2023-06-19 22:51:20 +0300

    created order routes

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ import { connectDB } from "./config/db.js"
 import customerRoute from './routes/customerRoute.js'
 import vendorRoute from './routes/vendorRoute.js'
 import menuRoute from './routes/menuRoute.js'
+import orderRoute from './routes/orderRoute.js'
 
 const app = express();
 const PORT = process.env.PORT || 5000
@@ -38,7 +39,7 @@ app.use(session({
   app.use("/api/v1/customer-auth", customerRoute)
   app.use("/api/v1/vendor-auth", vendorRoute)
   app.use("/api/v1/menu", menuRoute)
-
+  app.use("/api/v1/order", orderRoute)
 
 // Start the server
 app.listen(PORT, () => {
---
+++ routes/orderRoute.js
@@ -0,0 +1,12 @@
+import orderController from "../controllers/orderController.js";
+import express from "express"
+const router = express.Router()
+
+router.route("/create").post(orderController.createOrder)
+router.route("/vendor/:vendorId").get(orderController.getOrdersByVendor)
+router.route("/customer/:customerId").get(orderController.getOrdersByCustomer)
+router.route("/order/:id").get(orderController.getOrderById)
+router.route("/update/:id").put(orderController.updateOrderStatus)
+
+
+export default router
