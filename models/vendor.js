e2b1be5008753f04afb62afa72ca4f14d4f1c71b
munene-m <munenenjue18@gmail.com>
2023-06-24 17:21:28 +0300

    added  a rider's field to enable vendor to add their own riders

--- models/vendor.js
+++ models/vendor.js
@@ -1,46 +1,58 @@
 import mongoose from "mongoose";
-const vendorSchema = new mongoose.Schema({
+const vendorSchema = new mongoose.Schema(
+  {
     name: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     email: {
-        type: String,
-        required: true,
-        unique: true
+      type: String,
+      required: true,
+      unique: true,
     },
     password: {
-        type:String,
-        required: true
+      type: String,
+      required: true,
     },
     phoneNumber: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     location: {
-        type: {
-          type: String,
-          enum: ['Point'], // Specify the type as "Point"
-          required: true
-        },
-        coordinates: {
-          type: [Number], // Array of [longitude, latitude] values
-          required: true
-        }
-      },
-    openingHours: {
+      type: {
         type: String,
-        required: true
+        enum: ["Point"], // Specify the type as "Point"
+        required: true,
+      },
+      coordinates: {
+        type: [Number], // Array of [longitude, latitude] values
+        required: true,
+      },
+    },
+    openingHours: {
+      type: String,
+      required: true,
     },
     closingHours: {
-        type: String,
-        require: true
+      type: String,
+      require: true,
     },
     businessRegistration: {
-        data: Buffer,
-        contentType: String
-    }
+      data: Buffer,
+      contentType: String,
+    },
+    riders: [
+      {
+        riderId: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Rider",
+        },
+        name: String,
+        phoneNumber: String,
+      },
+    ],
+  },
+  { timestamps: true }
+);
 
-}, {timestamps: true})
-
-export default mongoose.model("Vendor", vendorSchema)
+export default mongoose.model("Vendor", vendorSchema);
