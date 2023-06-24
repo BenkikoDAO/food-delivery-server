852910265d1056b7b56a26fa313fce26e70781e8
munene-m <munenenjue18@gmail.com>
2023-06-24 17:20:49 +0300

    added vendor routes to handle get requests, adding riders and deleting

--- routes/vendorRoute.js
+++ routes/vendorRoute.js
@@ -6,5 +6,9 @@ router.route("/register").post(vendorController.createVendor)
 router.route("/login").post(vendorController.loginVendor)
 router.route("/reset-password").post(vendorController.resetPassword)
 router.route("/update-password").put(vendorController.updatePassword)
+router.route("/").get(vendorController.getVendors)
+router.route("/:id").get(vendorController.getVendor)
+router.route("/delete/:id").delete(vendorController.deleteVendor)
+router.route("/addRider").post(vendorController.addRider)
 
 export default router
