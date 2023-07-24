b52b5484fc4fc964c1a0757c0fcfd00bba704e68
munene-m <munenenjue18@gmail.com>
2023-07-24 08:46:20 +0300

    added middleware to customer and vendor routes

--- routes/customerRoute.js
+++ routes/customerRoute.js
@@ -1,11 +1,12 @@
 import customerController from "../controllers/customerController.js";
+import customerProtect from "../middleware/customerMiddleware.js";
 import express from "express";
 const router = express.Router()
 
 router.route("/register").post(customerController.createCustomer)
 router.route("/login").post(customerController.loginCustomer)
 router.route("/reset").post(customerController.resetPassword)
-router.route("/update-password").put(customerController.updatePassword)
+router.route("/update-password").put(customerProtect, customerController.updatePassword)
 router.route("/").get(customerController.getCustomers)
 router.route("/:id").get(customerController.getCustomer)
 
--- routes/vendorRoute.js
+++ routes/vendorRoute.js
@@ -1,4 +1,5 @@
 import vendorController from "../controllers/vendorController.js";
+import vendorProtect from "../middleware/vendorMiddleware.js";
 import express from "express";
 const router = express.Router()
 
@@ -6,10 +7,10 @@ router.route("/register").post(vendorController.createVendor)
 router.route("/login").post(vendorController.loginVendor)
 router.route("/reset-password").post(vendorController.resetPassword)
 router.route("/update-password").put(vendorController.updatePassword)
-router.route("/update-vendor/:id").put(vendorController.updateVendor)
+router.route("/update-vendor/:id").put(vendorProtect, vendorController.updateVendor)
 router.route("/").get(vendorController.getVendors)
 router.route("/:id").get(vendorController.getVendor)
-router.route("/delete/:id").delete(vendorController.deleteVendor)
-router.route("/addRider").post(vendorController.addRider)
+router.route("/delete/:id").delete(vendorProtect, vendorController.deleteVendor)
+router.route("/addRider").post(vendorProtect, vendorController.addRider)
 
 export default router
