c9bc8cfea0d73052784f8428159199a41718cfd8
munene-m <munenenjue18@gmail.com>
2023-08-06 18:48:41 +0300

    enabled file uploads by addin the upload middleware

--- routes/menuRoute.js
+++ routes/menuRoute.js
@@ -1,13 +1,15 @@
 import menuController from "../controllers/menuController.js";
+import vendorProtect from "../middleware/vendorMiddleware.js";
 import express from "express";
 import multer from "multer";
 const upload = multer({ dest: 'uploads/'})
 const router = express.Router()
 
 router.route("/:id").get(menuController.getMenuItem)
+router.route("/vendor/:vendorId").get(menuController.getMenuItemByVendor)
 router.route("/").get(menuController.getMenuItems)
-router.route("/delete/:id").delete(menuController.deleteMenuItem)
-router.route("/create").post(upload.single('image'), menuController.addMenuItem)
-router.route("/update/:id").put(upload.single('image'), menuController.updateMenuItem)
+router.route("/delete/:id").delete(vendorProtect, menuController.deleteMenuItem)
+router.route("/create").post(vendorProtect, upload.single('image'), menuController.addMenuItem)
+router.route("/update/:id").put(vendorProtect, upload.single('image'), menuController.updateMenuItem)
 
 export default router
