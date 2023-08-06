f3cb29471656645b857c0b3312fa701483a926f5
munene-m <munenenjue18@gmail.com>
2023-08-06 18:49:31 +0300

    added upload middleware and route to get riders by vendor and delete a rider

--- routes/riderRoute.js
+++ routes/riderRoute.js
@@ -1,12 +1,17 @@
-import { createRider, loginRider, resetPassword, updatePassword, getRider, getRiders } from "../controllers/riderController.js";
+import { createRider, loginRider, updateRider, deleteRider, resetPassword, updatePassword, getRider, getRiders, getRidersByVendor } from "../controllers/riderController.js";
 import express from "express";
+import multer from "multer";
+const upload = multer({ dest: 'uploads/'})
 const router = express.Router()
 
-router.route("/register").post(createRider)
+router.route("/register").post(upload.single('image'), createRider)
 router.route("/login").post(loginRider)
+router.route("/update/:id").put(upload.single('image'), updateRider)
 router.route("/reset").post(resetPassword)
 router.route("/update-password").put(updatePassword)
 router.route("/").get(getRiders)
+router.route("/riders/:vendorId").get(getRidersByVendor)
+router.route("/delete/:id").delete(deleteRider)
 router.route("/:id").get(getRider)
 
 export default router
