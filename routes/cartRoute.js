b2534677fb67f03a879685bf387ddcdb8fbaf9c5
munene-m <munenenjue18@gmail.com>
2023-08-17 00:01:53 +0300

    added route to update the personalized order

--- routes/cartRoute.js
+++ routes/cartRoute.js
@@ -1,4 +1,4 @@
-import { addCartItem, getCartItems, updateCart, deleteItem } from '../controllers/cartController.js'
+import { addCartItem, getCartItems, updateCart, updateExtraNote, deleteItem } from '../controllers/cartController.js'
 import customerProtect from '../middleware/customerMiddleware.js'
 import express from 'express'
 const router = express.Router()
@@ -6,6 +6,7 @@ const router = express.Router()
 router.route('/add').post(customerProtect,addCartItem)
 router.route('/items/:id').get(getCartItems)
 router.route('/update/:id').put(customerProtect,updateCart)
-router.route('/delete/:id').delete(deleteItem)
+router.route('/update-note/:id').put(customerProtect, updateExtraNote)
+router.route('/delete/:id').delete(customerProtect,deleteItem)
 
 export default router
