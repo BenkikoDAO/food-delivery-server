423fd82a8f823ed315b6a125f4c493a3a924ef95
munene-m <munenenjue18@gmail.com>
2023-08-27 00:09:30 +0300

    access req.body.message

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -3,12 +3,12 @@ import logger from "../helpers/logging.js";
 
 export async function handleCallback(req, res) {
     const { data } = req.body;
-    logger.info(req.body)
+    // logger.info(req.body)
   
     try {
       // Save the response data to your database (all responses, regardless of success)
       const paymentResponse = new Payment({
-        data: data,
+        data: data.message,
         timestamp: new Date(),
       });
   
