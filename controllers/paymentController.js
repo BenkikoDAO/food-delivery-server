f9ddd6f159a438e6c174e6c3faa12bae0ec222e0
munene-m <munenenjue18@gmail.com>
2023-08-27 15:24:38 +0300

    get the transaction and determine whether it is successful or not

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -11,7 +11,7 @@ export async function handleCallback(req, res) {
   
       await paymentResponse.save();
 
-      logger.info(`Payment response sent - ${paymentResponse.data}`)
+      logger.info(`Payment response sent - ${paymentResponse.data.Message}`)
   
       // Respond with the saved payment response data
       res.status(200).json({ message: "Callback response received", paymentResponse });
@@ -20,4 +20,18 @@ export async function handleCallback(req, res) {
       logger.error('Error occured while handling callback: ', error)
       res.status(500).json({ message: "An error occurred while handling callback" });
     }
+  }
+
+  export async function getCallbackResponse(req, res){
+    try {
+        const { transactionRef } = req.body
+
+        const response = await Payment.find({ transaction_reference: transactionRef })
+    
+        res.status(200).json({success: response.data.Success})
+    } catch (error) {
+        logger.error(error)
+        res.status(500).json({message: 'An error occured when fetching your response'})
+    }
+
   }
