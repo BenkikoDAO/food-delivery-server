99a4b56851d5ef47b4ceb440b272ca13c8e6da83
munene-m <munenenjue18@gmail.com>
2023-08-29 13:36:41 +0300

    added route to get orders by vendor

--- routes/orderRoute.js
+++ routes/orderRoute.js
@@ -1,12 +1,15 @@
 import orderController from "../controllers/orderController.js";
+import vendorProtect from "../middleware/vendorMiddleware.js";
+import customerProtect from "../middleware/customerMiddleware.js";
 import express from "express"
 const router = express.Router()
 
-router.route("/create").post(orderController.createOrder)
-router.route("/vendor/:vendorId").get(orderController.getOrdersByVendor)
+router.route("/create").post( customerProtect, orderController.createOrder)
+router.route("/vendor/orders").get(orderController.getOrdersByVendor)
 router.route("/customer/:customerId").get(orderController.getOrdersByCustomer)
 router.route("/order/:id").get(orderController.getOrderById)
 router.route("/update/:id").put(orderController.updateOrderStatus)
 router.route("/delete/:id").delete(orderController.deleteOrder)
+router.route("/delete-orders/all").delete(orderController.deleteOrders)
 
 export default router
