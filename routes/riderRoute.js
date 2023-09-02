1d296737199f2641682d5314fa01c57b53ab5c95
munene-m <munenenjue18@gmail.com>
2023-09-02 11:45:31 +0300

    added reset request endpoint

--- routes/riderRoute.js
+++ routes/riderRoute.js
@@ -1,4 +1,4 @@
-import { createRider, loginRider, updateRider, deleteRider, resetPassword, updatePassword, getRider, getRiders, getRidersByVendor } from "../controllers/riderController.js";
+import { createRider, loginRider, updateRider, deleteRider, requestResetPassword, updatePassword, getRider, getRiders, getRidersByVendor } from "../controllers/riderController.js";
 import express from "express";
 import multer from "multer";
 const upload = multer({ dest: 'uploads/'})
@@ -6,8 +6,8 @@ const router = express.Router()
 
 router.route("/register").post(upload.single('image'), createRider)
 router.route("/login").post(loginRider)
-router.route("/update/:id").put(upload.single('image'), updateRider)
-router.route("/reset").post(resetPassword)
+router.route("/update/:id").put(updateRider)
+router.route("/reset").post(requestResetPassword)
 router.route("/update-password").put(updatePassword)
 router.route("/").get(getRiders)
 router.route("/riders/:vendorId").get(getRidersByVendor)
