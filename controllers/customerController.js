9d016fc6f14183fa5cfc8dc2695af498e11f4f09
munene-m <munenenjue18@gmail.com>
2023-09-02 12:20:30 +0300

    use client url in reset

--- controllers/customerController.js
+++ controllers/customerController.js
@@ -6,6 +6,7 @@ import logger from "../helpers/logging.js";
 import customer from "../models/customer.js";
 
 let bcryptSalt = process.env.BCRYPT_SALT;
+const clientUrl = process.env.CLIENT_URL
 //Register customer
 export async function createCustomer(req, res) {
   try {
@@ -91,7 +92,7 @@ export async function loginCustomer(req, res) {
   }
 }
 
-export const resetPassword = async (req, res) => {
+export const requestResetPassword = async (req, res) => {
   const { email } = req.body;
 
   sgMail.setApiKey(process.env.SENDGRID_APIKEY);
@@ -105,18 +106,18 @@ export const resetPassword = async (req, res) => {
 
     if (!user) throw new Error("User does not exist");
 
-    const userId = user.id; // Get the user's ID
+    const userId = user._id; // Get the user's ID
 
     const resetToken = jwt.sign({ userId }, process.env.JWT_SECRET, {
       expiresIn: "1h",
     }); // Generate the reset token
 
-    const resetLink = `https://e-commerce-munene-m/reset-password?token=${resetToken}`; //remember to change this to a client side route with the form to reset credentials
+    const resetLink = `${clientUrl}/reset-password?token=${resetToken}`; //remember to change this to a client side route with the form to reset credentials
 
     const msg = {
       to: email,
       from: "macmunene364@gmail.com", //remember to change this to the official client side email
-      subject: "Reset Your Password",
+      subject: "Password reset for Mobile eats account",
       text: `Click the following link to reset your password: ${resetLink}`,
     };
     sgMail
@@ -125,45 +126,44 @@ export const resetPassword = async (req, res) => {
         res.status(200).json({ message: "Reset password email sent" });
       })
       .catch((error) => {
-        res.status(400).json({ error: "Failed to send reset password email" });
         console.error("Error sending reset password email:", error);
+        res.status(500).json({ error: "Failed to send reset password email" });
       });
   } catch (error) {
-    res.status(400).json({ error: "Failed to initiate password reset" });
+    res.status(500).json({ error: "Failed to initiate password reset" });
     console.log(error);
   }
 };
 
-export const updatePassword = async (req, res) => {
-  const { token, newPassword } = req.body; //in the client side, extract token from the reset link url and send it in a hidden input where you set value=<TOKEN>
-
+export async function changePassword(req, res) {
+  const { email, password } = req.body;
+  if(!email || !password){
+    return res.status(400).json({ message: "Please enter all the required fields" });
+  }
+  const customer = await Customer.findOne({ email });
+  
   try {
-    // Verify the token
-    const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
-
-    // Extract the user ID from the decoded token
-    const userId = decodedToken.userId;
-
-    // Find the user in the database by their ID
-    const user = await Customer.findOne({ _id: userId });
-
-    if (!user) {
-      throw new Error("User not found");
+    // Find the user by email
+    const customer = await Customer.findOne({ email });
+    if (!customer) {
+      return res.status(404).json({ error: "User not found" });
     }
+    // Hash the new password
+    const hashedPassword = await bcrypt.hash(password, Number(bcryptSalt));
 
     // Update the user's password
-    const hashedPassword = await bcrypt.hash(newPassword, Number(bcryptSalt));
-    user.password = hashedPassword;
-
-    // Save the updated user in the database
-    await user.save();
-
-    res.status(200).json({ message: "Password updated successfully" });
+    await Customer.findByIdAndUpdate(
+      customer._id,
+      { password: hashedPassword },
+      { new: true }
+    );
+    logger.info(`${customer.username} - changed password successfully`)
+    res.status(200).json({ message: "Password changed successfully." });
   } catch (error) {
-    res.status(400).json({ error: "Failed to update password" });
-    console.log(error);
+    logger.error(`Error changing password for customer - ${customer.email}`)
+    res.status(500).json({ error: "Failed to change password" });
   }
-};
+}
 
 export async function getCustomers(req, res) {
   try {
@@ -204,8 +204,8 @@ const generateToken = (id) => {
 export default {
   createCustomer,
   loginCustomer,
-  resetPassword,
-  updatePassword,
+  requestResetPassword,
+  changePassword,
   getCustomers,
   getCustomer,
 };
