f7dbd6acd1e00ef4ffa53911d49790dd9694e638
munene-m <munenenjue18@gmail.com>
2023-09-07 12:13:43 +0300

    update vendor specialties

--- controllers/vendorController.js
+++ controllers/vendorController.js
@@ -145,14 +145,14 @@ export async function updateVendor(req, res) {
       if(req.body.itemName){
         const dish = req.body.itemName; // Get the name to add from the request
 
-        if (specialties.includes(dish)) {
+        if (vendor.specialties.includes(dish)) {
           // Name is empty or already in specialties, do nothing
           res.status(400).json({ message: "Dish already in specialties." });
           return;
         }
   
         // Add the new name to specialties
-        specialties.push(dish);
+        vendor.specialties.push(dish);
       }
 
       if (req.file) {
