9d6c78f467a65efb219d2f34ffd87ab1f5e4ef52
munene-m <munenenjue18@gmail.com>
2023-09-11 18:24:17 +0300

    created redisclient file to handle connection to redis

---
+++ helpers/redisClient.js
@@ -0,0 +1,30 @@
+import logger from './logging.js';
+import { createClient } from 'redis';
+
+const redisClient = createClient({
+  url: process.env.REDIS_URL,
+});
+
+export const redisConnect = async () => {
+  try {
+    await redisClient.connect();
+    logger.info('Redis connected');
+  } catch (err) {
+    logger.error('Redis Error:', err);
+  }
+};
+
+export const redisDisconnect = async () => {
+  try {
+    await redisClient.disconnect();
+    logger.info('Redis disconnected');
+  } catch (err) {
+    logger.error('Redis Error:', err);
+  }
+};
+
+redisClient.on('error', error => {
+  logger.error('Redis Error:', error);
+});
+
+export default redisClient;
