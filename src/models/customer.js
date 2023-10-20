21cc12cec00ae865827dbb2d22ba75fcf30b63c9
munene-m <munenenjue18@gmail.com>
2023-10-20 21:18:47 +0300

    chore: add a token string to model for password reset

--- src/models/customer.js
+++ src/models/customer.js
@@ -1,32 +1,37 @@
 import mongoose from "mongoose";
-const customerSchema = new mongoose.Schema({
+const customerSchema = new mongoose.Schema(
+  {
     username: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     email: {
-        type: String,
-        required: true,
-        unique: true
+      type: String,
+      required: true,
+      unique: true,
     },
     password: {
-        type:String,
-        required: true
+      type: String,
+      required: true,
     },
     phoneNumber: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
+    },
+    token: {
+      type: String,
     },
     location: {
-        type: {
-          type: String,
-          enum: ['Point'], // Specify the type as "Point"
-        },
-        coordinates: {
-          type: [Number], // Array of [longitude, latitude] values
-        }
+      type: {
+        type: String,
+        enum: ["Point"], // Specify the type as "Point"
       },
+      coordinates: {
+        type: [Number], // Array of [longitude, latitude] values
+      },
+    },
+  },
+  { timestamps: true }
+);
 
-}, {timestamps: true})
-
-export default mongoose.model("Customer", customerSchema)
+export default mongoose.model("Customer", customerSchema);
